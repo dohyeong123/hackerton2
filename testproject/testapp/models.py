25ef0cba911710e66515305e32b470faa39dcd78
sudond <sudond@naver.com>
2019-08-09 01:21:11 +0900

    model, view

--- testproject/testapp/models.py
+++ testproject/testapp/models.py
@@ -1,10 +1,30 @@
 from django.db import models
 
 # Create your models here.
-class Blog(models.Model):
-    title = models.CharField(max_length=200)
-    pub_date = models.DateTimeField('date published')
-    body = models.TextField()
+class Category(models.Model):
+    categorys = models.CharField(max_length=20, unique=True)
 
+
+class Thumbnail(models.Model):
+    cate_number = models.ForeignKey(Category, on_delete=models.CASCADE, null=True)
+    urlId = models.CharField(max_length=100)
+    majorTitle = models.CharField(max_length=100)
+    subTitle = models.CharField(max_length=100)
+    cateName = models.CharField(max_length=100)
+    cateDetailName = models.CharField(max_length=100)
+
+    created = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['-created']
+    
+    
     def __str__(self):
-        return self.title
+        return self.cateName + " " + self.cateDetailName
+
+
+    
+
+
+
+    
--- testproject/testapp/views.py
+++ testproject/testapp/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-
+from .models import Thumbnail
 # Create your views here.
 def home(request):
     return render(request, 'testapp/home.html')
@@ -9,4 +9,31 @@ def logout(request):
     return render(request,'testapp/logout.html')
 def signup(request):
     return render(request,'testapp/signup.html')
-
+def cate_trend(request):
+    thumbnails = Thumbnail.objects
+    checkname = 1
+    category = 'trend'
+    cate_real_name = request.GET.get('sport_category')
+    
+    if cate_real_name == 'trend':
+        checkname = 1
+        category = '#트렌드운동'
+    
+    elif cate_real_name == 'difficulty':
+        checkname = 2
+        category = '#난이도별'
+    elif cate_real_name == 'genre':
+        checkname = 3
+        category = '#장르'
+    elif cate_real_name == 'guideline':
+        checkname = 4
+        category = '#헬스기구사용법'
+    elif cate_real_name == 'sub1':
+        checkname = 5
+        category = '#저쩌구별'
+    elif cate_real_name == 'sub2':
+        checkname = 6
+        category = '#쟈쩌구별'
+    category_filter = thumbnails.objects.filter(cate_number=checkname)
+    return render(request, 'testapp/cate_trend.html', 
+{'thumbnails':thumbnails, 'category':category, 'category_filter':category_filter })
